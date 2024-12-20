554bf0bb89c8504b60f3c29b03162e20f0c06993
Blxys1 <balkis.ferjani@esprit.tn>
2024-12-20 15:25:16 +0100

    last update

--- README.md
+++ README.md
@@ -1,25 +1,77 @@
-# ProjetCAPI
+# Planning Poker Application
 
-issues to be fixed: 
-FIXED -game.py : start game  line 82
-when the tasks arent all validated the game stops yet it should restart the tasks that aren't validated yet so it prints correctly in the backlog 
+## Description
+L'application Planning Poker est un outil conçu pour faciliter l'estimation collaborative des fonctionnalités d'un backlog, en suivant les principes agiles. Elle offre une interface utilisateur simple, des règles flexibles de vote, et des fonctionnalités pour sauvegarder et charger les parties en cours.
 
-- FIXED boucle quand le jeu se termine il faut retourner au menu
+## Fonctionnalités Principales
 
--FIXED  the charge a game ( restarting another game isnt valid ) not functionning 
-- need to add sessions fix bugs 
+### 1. Menu Principal
+- **Créer une nouvelle partie** :
+  - Saisie du nombre de joueurs.
+  - Attribution de pseudonymes.
+  - Choix du mode de jeu parmi les options disponibles (strict, moyenne, médiane, majorité absolue, majorité relative).
+- **Charger une partie** :
+  - Chargement d'un fichier JSON contenant l'état d'une partie sauvegardée.
+
+### 2. Système de Vote
+- Modes de jeu disponibles :
+  - **Mode Strict** : Vote jusqu'à ce qu'un consensus soit atteint.
+  - **Mode Moyenne** : Utilisation de la moyenne des votes après le premier tour.
+  - **Mode Médiane** : Calcul de la médiane des votes pour valider une fonctionnalité.
+  - **Mode Majorité Absolue** : Validation si plus de 50% des joueurs votent pour la même valeur.
+  - **Mode Majorité Relative** : Validation si une valeur obtient plus de votes que les autres.
+- Gestion des fonctionnalités non validées pour un nouveau tour de vote.
+
+### 3. Sauvegarde et Chargement
+- Sauvegarde automatique après chaque action critique (vote, création de partie).
+- Chargement d'une partie depuis un fichier JSON.
+
+### 4. Rapport Final
+- Génération automatique du rapport à la fin de la partie.
+- Affichage des fonctionnalités validées et des estimations collectées.
+
+## Prérequis
+- **Python 3.8+**
+- **Bibliothèques Python** :
+  - `tkinter`
+  - `pytest` (pour les tests unitaires)
+
+## Installation
+1. Clonez le dépôt :
+   ```bash
+   git clone <url-du-repo>
+   ```
+2. Accédez au répertoire du projet :
+   ```bash
+   cd planning-poker
+   ```
+3. Installez les dépendances nécessaires (si applicable).
+
+## Utilisation
+1. Lancez l'application :
+   ```bash
+   python app.py
+   ```
+2. Suivez les instructions dans l'interface pour commencer une nouvelle partie.
+
+## Tests Unitaires
+- Les tests unitaires sont disponibles dans le fichier `test_game.py`.
+- Exécutez les tests avec la commande suivante :
+  ```bash
+  pytest test_game.py
+  ```
+
+## Documentation
+- La documentation est générée automatiquement avec Doxygen.
+- Pour générer la documentation, exécutez :
+  ```bash
+  doxygen Doxyfile
+  ```
+
+## Contributeurs
+- Balkis Ferjani
+- Yasmine Maddouri
 
 
-- FIXED create unitairy tests 
-
--  FIXED create the documentations
-FIXED fix the : when not validated it need to re ask again  
-FIXED fix the final report i want to display it 
-
-------------------------------------------------------------------------
-- correct the language all to eng or all to fr 
-- cafe card 
-- first round strict rule
--input fonctionalities 
 
 
--- app.py
+++ app.py
@@ -16,16 +16,16 @@ class PlanningPokerApp:
 
         # Style
         self.style = ttk.Style()
-        self.style.configure("TButton", font=("Arial", 12), padding=10)
-        self.style.configure("TLabel", font=("Arial", 14))
+        self.style.configure("TButton", font=("Arial", 16), padding=15)
+        self.style.configure("TLabel", font=("Arial", 20))
 
         self.game = None
 
         # Frames
-        self.main_menu_frame = tk.Frame(root, bg="#f0f0f0")
-        self.setup_frame = tk.Frame(root, bg="#f0f0f0")
-        self.voting_frame = tk.Frame(root, bg="#f0f0f0")
-        self.results_frame = tk.Frame(root, bg="#f0f0f0")
+        self.main_menu_frame = tk.Frame(root, bg="#8699c2")
+        self.setup_frame = tk.Frame(root, bg="#8699c2")
+        self.voting_frame = tk.Frame(root, bg="#8699c2")
+        self.results_frame = tk.Frame(root, bg="#8699c2")
 
         # Main menu
         self.initialize_main_menu()
@@ -34,10 +34,10 @@ class PlanningPokerApp:
 
     def initialize_main_menu(self):
         self.main_menu_frame.pack(fill="both", expand=True)
-        tk.Label(self.main_menu_frame, text="Welcome to Planning Poker", font=("Arial", 24, "bold"), bg="#f0f0f0").pack(pady=20)
-        ttk.Button(self.main_menu_frame, text="New Game", command=self.new_game).pack(pady=10)
-        ttk.Button(self.main_menu_frame, text="Load Game", command=self.load_game).pack(pady=10)
-        ttk.Button(self.main_menu_frame, text="Exit", command=self.root.quit).pack(pady=10)
+        tk.Label(self.main_menu_frame, text="Bienvenue à Poker Planning", font=("Arial", 30, "bold"), bg="#a81125").pack(pady=20)
+        ttk.Button(self.main_menu_frame, text="Nouvelle Partie", command=self.new_game).pack(pady=15)
+        ttk.Button(self.main_menu_frame, text="Charger Partie", command=self.load_game).pack(pady=15)
+        ttk.Button(self.main_menu_frame, text="Quitter", command=self.root.quit).pack(pady=15)
 
     def switch_frame(self, frame):
         for widget in self.root.winfo_children():
@@ -55,52 +55,52 @@ class PlanningPokerApp:
         for widget in self.setup_frame.winfo_children():
             widget.destroy()
 
-        tk.Label(self.setup_frame, text="New Game Setup", font=("Arial", 18, "bold"), bg="#f0f0f0").pack(pady=20)
+        tk.Label(self.setup_frame, text="Nouveau Jeu", font=("Arial", 20, "bold"), bg="#a81125").pack(pady=20)
 
-        tk.Label(self.setup_frame, text="Enter Number of Players:", bg="#f0f0f0").pack(pady=10)
+        tk.Label(self.setup_frame, text="Enter Nombre De Joueurs ( Attention Supérieur à 1 ! )",  font=("Arial", 12), bg="#456499").pack(pady=20)
         num_players_var = tk.IntVar()
-        ttk.Entry(self.setup_frame, textvariable=num_players_var).pack(pady=5)
+        ttk.Entry(self.setup_frame, textvariable=num_players_var).pack(pady=10)
 
-        tk.Label(self.setup_frame, text="Enter Rules (strict, average, median, absolute_majority, relative_majority):", bg="#f0f0f0").pack(pady=10)
+        tk.Label(self.setup_frame, text="Choisir Règles (strict, average, median, absolute_majority, relative_majority):", font=("Arial", 12), bg="#456499").pack(pady=20)
         rules_var = tk.StringVar()
-        ttk.Entry(self.setup_frame, textvariable=rules_var).pack(pady=5)
+        ttk.Entry(self.setup_frame, textvariable=rules_var).pack(pady=20)
 
         def setup_players():
             num_players = num_players_var.get()
             rules = rules_var.get()
             if num_players <= 1:
-                self.show_popup("Error", "Invalid number of players.")
+                self.show_popup("Error", "Nombre Invalide! Entrez un nombre valid.")
                 return
             if rules not in ["strict", "average", "median", "absolute_majority", "relative_majority"]:
-                self.show_popup("Error", "Invalid rules.")
+                self.show_popup("Error", "Règle Invalide! Entrer une règle valide.")
                 return
 
             self.game = Game(num_players=num_players, rules=rules)
             self.collect_player_pseudonyms(num_players)
 
-        ttk.Button(self.setup_frame, text="Next", command=setup_players).pack(pady=20)
+        ttk.Button(self.setup_frame, text="Suivant", command=setup_players).pack(pady=20)
 
     def collect_player_pseudonyms(self, num_players):
         for widget in self.setup_frame.winfo_children():
             widget.destroy()
 
-        tk.Label(self.setup_frame, text="Player Setup", font=("Arial", 18, "bold"), bg="#f0f0f0").pack(pady=20)
+        tk.Label(self.setup_frame, text="Choix Joueur", font=("Arial", 20, "bold"), bg="#a81125").pack(pady=20)
 
         self.pseudonym_vars = []
         for i in range(num_players):
-            tk.Label(self.setup_frame, text=f"Enter pseudonym for Player {i + 1}:", bg="#f0f0f0").pack(pady=5)
+            tk.Label(self.setup_frame, text=f"Enter pseudonym pour Joueur numero {i + 1}:", font=("Arial", 12),  bg="#456499").pack(pady=10)
             pseudo_var = tk.StringVar()
             self.pseudonym_vars.append(pseudo_var)
-            ttk.Entry(self.setup_frame, textvariable=pseudo_var).pack(pady=5)
+            ttk.Entry(self.setup_frame, textvariable=pseudo_var).pack(pady=10)
 
-        ttk.Button(self.setup_frame, text="Start Game", command=self.start_game).pack(pady=20)
+        ttk.Button(self.setup_frame, text="Commencer le Jeu", command=self.start_game).pack(pady=20)
 
     def start_game(self):
         self.game.players = []
         for pseudo_var in self.pseudonym_vars:
             pseudo = pseudo_var.get()
             if not pseudo:
-                self.show_popup("Error", "Pseudonym cannot be empty.")
+                self.show_popup("Error", "Pseudonym ne peut pas être vide.")
                 return
             self.game.players.append(Player(pseudo))
 
@@ -112,7 +112,7 @@ class PlanningPokerApp:
             self.game.load_game_state("data/game_state.json")
             self.start_voting()
         except FileNotFoundError:
-            self.show_popup("Error", "No saved game found.")
+            self.show_popup("Error", "Aucune sauvegarde trouvée.")
         except Exception as e:
             self.show_popup("Error", f"Error: {str(e)}")
 
@@ -125,20 +125,20 @@ class PlanningPokerApp:
             self.game.current_feature = self.game.backlog[0]
 
         current_feature = self.game.current_feature
-        tk.Label(self.voting_frame, text=f"Current Feature: {current_feature['description']}", 
-                 font=("Arial", 18, "bold"), bg="#f0f0f0").pack(pady=20)
+        tk.Label(self.voting_frame, text=f"Tache actuelle : {current_feature['description']}", 
+                 font=("Arial", 18, "bold"), bg="#456499").pack(pady=20)
 
         self.vote_inputs = {}
         for player in self.game.players:
-            frame = tk.Frame(self.voting_frame, bg="#f0f0f0")
+            frame = tk.Frame(self.voting_frame, bg="#456499")
             frame.pack(pady=10)
-            tk.Label(frame, text=f"{player.pseudo}'s vote: ", font=("Arial", 14), bg="#f0f0f0").pack(side="left")
-            tk.Label(frame, text=f"Available Cards: {', '.join(player.cards)}", font=("Arial", 12), bg="#f0f0f0").pack(pady=5)
+            tk.Label(frame, text=f"Le vote de {player.pseudo}: ", bg="#f0f0f0").pack(side="left")
+            tk.Label(frame, text=f"Carte Disponible: {', '.join(player.cards)}",  bg="#f0f0f0").pack(pady=5)
             var = tk.StringVar()
             self.vote_inputs[player.pseudo] = var
             ttk.Entry(frame, textvariable=var).pack(side="left")
 
-        ttk.Button(self.voting_frame, text="Submit Votes", command=self.submit_votes).pack(pady=20)
+        ttk.Button(self.voting_frame, text="Valider Vote", command=self.submit_votes).pack(pady=30)
 
     def submit_votes(self):
         try:
@@ -151,7 +151,7 @@ class PlanningPokerApp:
                     return
 
             if self.game.process_votes():
-                self.show_popup("Success", f"Feature '{self.game.current_feature['description']}' validated!")
+                self.show_popup("Succès", f"Tache Actuelle:  '{self.game.current_feature['description']}' validée!")
                 
                 # Move to the next feature without removing from backlog
                 current_index = self.game.backlog.index(self.game.current_feature)
@@ -170,7 +170,7 @@ class PlanningPokerApp:
                     self.game.save_final_report("data/final_report.json")
                     self.display_final_report()
             else:
-                self.show_popup("Info", f"Feature '{self.game.current_feature['description']}' not validated. Revoting...")
+                self.show_popup("Info", f"Tache Actuelle: '{self.game.current_feature['description']}' non validé. Veuillez refaire le vote...")
                 self.game.reset_votes()
                 self.start_voting()
         except Exception as e:
@@ -179,13 +179,13 @@ class PlanningPokerApp:
     def display_final_report(self):
         self.switch_frame(self.results_frame)
 
-        tk.Label(self.results_frame, text="Final Report", font=("Arial", 18, "bold"), bg="#f0f0f0").pack(pady=20)
+        tk.Label(self.results_frame, text=" Rapport Final", font=("Arial", 20, "bold"), bg="#a81125").pack(pady=30)
 
         for feature in self.game.backlog:
             status = "Validated" if feature.get("validated", False) else "Not Validated"
-            tk.Label(self.results_frame, text=f"Feature: {feature['description']}, Status: {status}",   bg="#f0f0f0").pack(pady=5)
+            tk.Label(self.results_frame, text=f"Tache Actuelle: {feature['description']}, Status: {status}", font=("Arial", 16) , bg="#f0f0f0").pack(pady=10)
 
-        ttk.Button(self.results_frame, text="Back to Main Menu", command=lambda: self.switch_frame(self.main_menu_frame)).pack(pady=20)
+        ttk.Button(self.results_frame, text="Retour Menu Principal", command=lambda: self.switch_frame(self.main_menu_frame)).pack(pady=20)
 
 
 if __name__ == "__main__":
